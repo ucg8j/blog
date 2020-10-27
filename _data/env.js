ebc7ca1eb9d172d2cef68130c40aea34eeda7af4
ucg8j <lukesingham@gmail.com>
2020-10-27 12:47:32 +0000

    Update env.js

--- _data/env.js
+++ _data/env.js
@@ -2,7 +2,7 @@
 
 const environment = process.env.ELEVENTY_ENV;
 const PROD_ENV = 'prod';
-const prodUrl = 'https://your-production.url';
+const prodUrl = 'https://lukesingham.com';
 const devUrl = 'http://localhost:8080';
 const baseUrl = environment === PROD_ENV ? prodUrl : devUrl;
 const isProd = environment === PROD_ENV;
@@ -16,8 +16,8 @@ const dir = {
 }
 
 module.exports = {
-  siteName: 'your site name',
-  author: 'your name',
+  siteName: 'read and write',
+  author: 'Luke Singham',
   environment,
   isProd,
   folder,
